8e7c221033bcdba9490114cb738b9ae2c47fb7aa
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-10-13 23:59:39 +0530

    navbar

---
+++ src/Components/DropDown.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Dropdown = ({
+  label,
+  items,
+  border = "border ",
+  textColor = "text-orange-500",
+  paddingY = "py-2",
+  paddingX = "px-5",
+  hoverbg = "hover:bg-orange-500",
+  hovertext = "hover:text-white "
+
+}) => {
+  return (
+    <div className="relative group inline-block">
+      <button
+        className={`${border} border-orange-500 ${hovertext}  ${hoverbg} rounded-lg ${paddingY} ${paddingX} ${textColor}  font-semibold flex items-center`}
+      >
+        {label}
+        <span className="ml-2 transition-transform duration-300 group-hover:rotate-180">
+          <svg
+            className="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 9l-7 7-7-7"
+            ></path>
+          </svg>
+        </span>
+      </button>
+
+      <div className="absolute hidden group-hover:block mt-2 w-48 bg-white shadow-lg border border-gray-200 rounded-md">
+        <ul>
+          {items.map((item, index) => (
+            <li
+              key={index}
+              className="px-4 py-2 hover:bg-orange-100 cursor-pointer"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,65 @@
 import React from "react";
 import logo from "../images/vedantu-logo.svg";
 import call from "../images/call.png";
-
+import cart from "../images/shopping-cart.png";
+import DropDown from "../Components/DropDown";
 const Navbar = () => {
+  const courseItems = ["Course 1", "Course 2", "Course 3"];
   return (
     <>
       <div className="flex justify-between h-[80px] items-center px-[60px] border-b ">
         <div>
           <img src={logo} alt="" />
         </div>
-        <div>Courses</div>
 
-        <ul className=" flex gap-[20px]">
-          <li>Courses by kids</li>
-          <li>Free study material</li>
-          <li>Offline Centers</li>
-          <li>More</li>
-          <li>
-            <img src="" alt="" />
+        <DropDown label="Courses" items={courseItems} />
+
+        <div className="flex gap-[22px]">
+          <DropDown
+            label="Course for kids"
+            border="none"
+            textColor="black"
+            paddingX="0"
+            paddingY="0"
+            items={courseItems}
+            hoverbg=""
+            hovertext=""
+          />
+          <DropDown
+            label="Free study material"
+            border="none"
+            textColor="black"
+            paddingX="0"
+            paddingY="0"
+            hoverbg=""
+            hovertext=""
+            items={courseItems}
+          />
+          <DropDown
+            label="Offline centers"
+            border="none"
+            textColor="black"
+            paddingX="0"
+            paddingY="0"
+            hoverbg=""
+            hovertext=""
+            items={courseItems}
+          />
+          <DropDown
+            label="More"
+            border="none"
+            textColor="black"
+            paddingX="0"
+            paddingY="0"
+            hoverbg=""
+            hovertext=""
+            items={courseItems}
+          />
+          <div className=" flex ">
+            <img src={cart} alt="" className="h-[25px] w-[25px] " />
             Store
-          </li>
-        </ul>
+          </div>
+        </div>
 
         <div className="flex gap-6">
           <div className="flex items-center gap-2">
@@ -33,7 +72,7 @@ const Navbar = () => {
             </div>
           </div>
 
-          <button className="border border-orange-500 rounded-lg px-[20px] py-[4px] text-orange-500 font-semibold">
+          <button className="border border-orange-500 rounded-lg px-5 py-1  text-orange-500 font-semibold">
             Sign In
           </button>
         </div>
